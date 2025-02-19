66606fcc56b804d957d411cd5aa0cbeb18337bd7
BALKNAoppa <baterdene207@gmail.com>
2025-02-19 10:40:46 +0800

    someupdates

--- routes/users-router.js
+++ routes/users-router.js
@@ -7,11 +7,3 @@ export const userRouter = express.Router();
 userRouter.get("/", getUsers);
 
 userRouter.post("/", createUser);
-// userRouter.post('/', async (req, res) => {
-//   try {
-//     const newUser = await createUser(req.body);
-//     res.status(201).json(newUser);
-//   } catch (error) {
-//     res.status(500).json({ message: 'Error creating user', error: error.message });
-//   }
-// });
